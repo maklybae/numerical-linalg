92f9c1d0dad72d0175d26fbdb45f8c9206fa1bdc
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-01-15 19:33:49 +0300

    feat: :sparkles: basic ctors and dtor
    needs to implement rule of five cuz default move of sizes leave object not valid

--- src/matrix.h
+++ src/matrix.h
@@ -4,6 +4,8 @@
 #include <complex>
 #include <concepts>
 #include <type_traits>
+#include <utility>
+#include <vector>
 
 namespace linalg {
 
@@ -19,8 +21,42 @@ constexpr bool kIsComplexV = IsComplex<T>::value;
 template <typename T>
 concept FloatingOrComplexType = std::is_floating_point_v<T> || kIsComplexV<T>;
 
-template <FloatingOrComplexType T>
-class Matrix {};
+template <FloatingOrComplexType Scalar>
+class Matrix {
+ private:
+  using Container = std::vector<Scalar>;
+
+ public:
+  using SizeT = std::size_t;
+
+  Matrix() = default;
+
+  Matrix(const Matrix&) = default;
+
+  // Needs to leave other in valid state.
+  Matrix(Matrix&& other) noexcept
+      : rows_{std::exchange(other.rows_, 0)}
+      , cols_{std::exchange(other.cols_, 0)}
+      , data_{std::exchange(other.data_, {})} {}
+
+  Matrix& operator=(const Matrix&) = default;
+
+  // Needs to leave other in valid state.
+  Matrix& operator=(Matrix&& other) noexcept {
+    Matrix tmp = std::move(other);
+    std::swap(rows_, other.rows_);
+    std::swap(cols_, other.cols_);
+    std::swap(data_, other.data_);
+    return *this;
+  }
+
+  ~Matrix() = default;
+
+ private:
+  SizeT rows_{};
+  SizeT cols_{};
+  Container data_{};
+};
 }  // namespace linalg
 
 #endif
