8299480db26951be4efddf801c9190f1d52e0c84
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-01-18 00:05:02 +0300

    feat: :sparkles: more ctors and access operator()
    zero size-only ctor and std::initializer_list ctor

--- src/matrix.h
+++ src/matrix.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <complex>
 #include <concepts>
+#include <initializer_list>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -43,14 +44,27 @@ class Matrix {
       , cols_{std::exchange(other.cols_, 0)}
       , data_{std::exchange(other.data_, {})} {}
 
+  Matrix(SizeT rows, SizeT cols) : rows_{rows}, cols_{cols}, data_(rows * cols) {}
+
+  Matrix(std::initializer_list<std::initializer_list<Scalar>> list) {
+    rows_ = list.size();
+    cols_ = list.begin()->size();
+    data_.reserve(rows_ * cols_);
+
+    for (auto row : list) {
+      assert(row.size() == cols_ && "All rows should have the same size");
+      data_.insert(data_.end(), row);
+    }
+  }
+
   Matrix& operator=(const Matrix&) = default;
 
   // Needs to leave other in valid state.
   Matrix& operator=(Matrix&& other) noexcept {
     Matrix tmp = std::move(other);
-    std::swap(rows_, other.rows_);
-    std::swap(cols_, other.cols_);
-    std::swap(data_, other.data_);
+    std::swap(rows_, tmp.rows_);
+    std::swap(cols_, tmp.cols_);
+    std::swap(data_, tmp.data_);
 
     assert(other.rows_ == 0 && "Rows of moved matrix should be equal 0");
     assert(other.cols_ == 0 && "Cols of moved matrix should be equal 0");
@@ -70,6 +84,18 @@ class Matrix {
 
   bool Empty() const {
     return data_.empty();
+  }
+
+  Scalar& operator()(SizeT row, SizeT col) {
+    assert(row < rows_ && "Row index out of bounds");
+    assert(col < cols_ && "Col index out of bounds");
+    return data_[row * cols_ + col];
+  }
+
+  const Scalar& operator()(SizeT row, SizeT col) const {
+    assert(row < rows_ && "Row index out of bounds");
+    assert(col < cols_ && "Col index out of bounds");
+    return data_[row * cols_ + col];
   }
 
  private:
