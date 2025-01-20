d138685f9a6dc1d9e47253a7d62509a289609aca
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-01-20 18:14:17 +0300

    feat: :sparkles: scalars approx equal functions
    for std::floating_point and std::complex

--- src/scalars.h
+++ src/scalars.h
@@ -20,4 +20,18 @@ concept FloatingOrComplexType = std::is_floating_point_v<T> || kIsComplexV<T>;
 
 }  // namespace linalg::types
 
+namespace linalg::scalar_utils {
+
+template <std::floating_point T>
+bool ApproxEqual(T lhs, T rhs) {
+  return std::abs(lhs - rhs) < std::numeric_limits<T>::epsilon();
+}
+
+template <std::floating_point T>
+bool ApproxEqual(std::complex<T> lhs, std::complex<T> rhs) {
+  return ApproxEqual(lhs.real(), rhs.real()) && ApproxEqual(lhs.imag(), rhs.imag());
+}
+
+}  // namespace linalg::scalar_utils
+
 #endif
---
+++ tests/scalars_test.cpp
@@ -0,0 +1,33 @@
+#include "../src/scalars.h"
+
+#include <gtest/gtest.h>
+
+TEST(ScalarTypesConcepts, StaticAssertions) {
+  static_assert(linalg::types::FloatingOrComplexType<float>);
+  static_assert(linalg::types::FloatingOrComplexType<double>);
+  static_assert(linalg::types::FloatingOrComplexType<long double>);
+  static_assert(linalg::types::FloatingOrComplexType<std::complex<float>>);
+  static_assert(linalg::types::FloatingOrComplexType<std::complex<double>>);
+  static_assert(linalg::types::FloatingOrComplexType<std::complex<long double>>);
+}
+
+TEST(ScalarUtils, ApproxEqual) {
+  // Exact equality.
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0, 1.0));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0f, 1.0f));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0L, 1.0L));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(std::complex{1.0, 1.0}, std::complex{1.0, 1.0}));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(std::complex{1.0f, 1.0f}, std::complex{1.0f, 1.0f}));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(std::complex{1.0L, 1.0L}, std::complex{1.0L, 1.0L}));
+
+  // Approximate equality.
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0, 1.0 + std::numeric_limits<double>::epsilon() / 2));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0f, 1.0f + std::numeric_limits<float>::epsilon() / 2));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(1.0L, 1.0L + std::numeric_limits<long double>::epsilon() / 2));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(std::complex{1.0, 1.0},
+                                                std::complex{1.0 + std::numeric_limits<double>::epsilon() / 2, 1.0}));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(std::complex{1.0f, 1.0f},
+                                                std::complex{1.0f + std::numeric_limits<float>::epsilon() / 2, 1.0f}));
+  EXPECT_TRUE(linalg::scalar_utils::ApproxEqual(
+      std::complex{1.0L, 1.0L}, std::complex{1.0L + std::numeric_limits<long double>::epsilon() / 2, 1.0L}));
+}
