1687dc40b9ffc1bc19cc32f28d80a09a78469476
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-02-18 21:53:33 +0300

    feat: :sparkles: Matrix concepts
    with Mutable matrix

--- src/matrix_types.h
+++ src/matrix_types.h
@@ -1,6 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <type_traits>
+
 #include "types_details.h"
 
 namespace linalg::view {
@@ -20,23 +22,40 @@ using ConstMatrixView = view::BaseMatrixView<Scalar, types::ConstnessEnum::kCons
 }  // namespace linalg
 
 namespace linalg::types::details {
-template <typename T>
+template <typename Scalar>
 struct IsMatrix : std::false_type {};
 
-template <typename T>
-struct IsMatrix<Matrix<T>> : std::true_type {};
+template <typename Scalar>
+struct IsMatrix<Matrix<Scalar>> : std::true_type {};
 
-template <typename T>
-struct IsMatrix<MatrixView<T>> : std::true_type {};
+template <typename Scalar>
+struct IsMatrix<MatrixView<Scalar>> : std::true_type {};
 
-template <typename T>
-struct IsMatrix<ConstMatrixView<T>> : std::true_type {};
+template <typename Scalar>
+struct IsMatrix<ConstMatrixView<Scalar>> : std::true_type {};
 
-template <typename T>
-constexpr bool kIsMatrixV = IsMatrix<T>::value;
+template <typename Scalar>
+constexpr bool kIsMatrixV = IsMatrix<Scalar>::value;
 
-template <typename T>
-concept MatrixType = kIsMatrixV<T>;
+template <typename Scalar>
+struct IsMutableMatrix : std::false_type {};
+
+template <typename Scalar>
+struct IsMutableMatrix<Matrix<Scalar>> : std::true_type {};
+
+template <typename Scalar>
+struct IsMutableMatrix<MatrixView<Scalar>> : std::true_type {};
+
+template <typename Scalar>
+constexpr bool kIsMutableMatrixV = IsMutableMatrix<Scalar>::value;
 }  // namespace linalg::types::details
+
+namespace linalg::types {
+template <typename Scalar>
+concept MatrixType = details::kIsMatrixV<Scalar>;
+
+template <typename Scalar>
+concept MutableMatrixType = details::kIsMutableMatrixV<Scalar>;
+}  // namespace linalg::types
 
 #endif
