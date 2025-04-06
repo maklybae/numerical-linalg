e72fecd51ab6cc74ab05336be195671c2b4dc82e
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-04-06 11:43:44 +0300

    feat: :sparkles: right givens and functions naming

--- src/givens.h
+++ src/givens.h
@@ -3,7 +3,6 @@
 
 #include <cassert>
 #include <complex>
-#include <concepts>
 
 #include "matrix_types.h"
 #include "scalar_types.h"
@@ -17,37 +16,13 @@ struct GivensRotationParams {
   Scalar s;  // sin
 };
 
-template <std::floating_point Scalar>
-GivensRotationParams<Scalar> GetZeroingGivensRotationParams(Scalar a, Scalar b) {
-  // TODO ?: assert a != 0 || b != 0 (rotation is useless if both are zero)
-
-  if (detail::ApproxZero(b)) {
-    return {Scalar{1}, Scalar{0}};
-  }
-
-  if (detail::ApproxZero(a)) {
-    return {Scalar{0}, Scalar{1}};
-  }
-
-  if (std::abs(b) >= std::abs(a)) {
-    auto t = -a / b;
-    auto s = Scalar{1} / std::sqrt(Scalar{1} + t * t);
-    return {s * t, s};
-  }
-
-  auto t = -b / a;
-  auto c = Scalar{1} / std::sqrt(Scalar{1} + t * t);
-  return {c, c * t};
-}
-
 // We will use Givens rotations more with real numbers, since output of our reduction to tridiagonal form, after
 // postprocessing, yields a real-valued tridiagonal symmatric matrix.
-template <std::floating_point UnderlyingScalar>
-GivensRotationParams<std::complex<UnderlyingScalar>> GetZeroingGivensRotationParams(std::complex<UnderlyingScalar> a,
-                                                                                    std::complex<UnderlyingScalar> b) {
+template <detail::FloatingOrComplexType Scalar>
+GivensRotationParams<Scalar> GetZeroingFirstGivensRotationParams(Scalar a, Scalar b) {
   auto sqrt_norm = std::sqrt(std::norm(a) + std::norm(b));
   if (detail::ApproxZero(sqrt_norm)) {
-    return {std::complex<UnderlyingScalar>{1}, std::complex<UnderlyingScalar>{0}};
+    return {Scalar{1}, Scalar{0}};
   }
 
   return {a / sqrt_norm, -b / sqrt_norm};
@@ -75,6 +50,27 @@ void ApplyGivensRotationLeft(MatrixT& matrix, GivensRotationParams<typename Matr
   }
 }
 
+template <detail::MutableMatrixType MatrixT>
+void ApplyGivensRotationRight(MatrixT& matrix, GivensRotationParams<typename MatrixT::value_type> params, Index col_i,
+                              Index col_j) {
+  using Scalar = typename MatrixT::value_type;
+
+  assert(col_i != col_j && "Columns should be different");
+  assert(col_i < matrix.Cols() && col_j < matrix.Cols() && "Columns should be in range");
+
+  for (Index i = 0; i < matrix.Rows(); ++i) {
+    Scalar tmp_i = matrix(i, col_i);
+    Scalar tmp_j = matrix(i, col_j);
+
+    if constexpr (detail::kIsComplexV<Scalar>) {
+      matrix(i, col_i) = std::conj(params.c) * tmp_i - std::conj(params.s) * tmp_j;
+      matrix(i, col_j) = params.s * tmp_i + params.c * tmp_j;
+    } else {
+      matrix(i, col_i) = params.c * tmp_i - params.s * tmp_j;
+      matrix(i, col_j) = params.s * tmp_i + params.c * tmp_j;
+    }
+  }
+}
 }  // namespace linalg
 
 #endif  // GIVENS_H
